2635b2f234502517ed48f9fd1476f7ed8bbe02c1
BlyatTukan <Overnex03@gmail.com>
2023-05-05 00:04:24 +0200

    Rework 0.1

--- obsluga.php
+++
@@ -1,7 +0,0 @@
-<?php
-    alert("Uwaga Rebelianci!\n\nTen Tekst jest zapisany w obsluga.php");
-
-    function alert($msg) {
-        echo "<script type='text/javascript'>alert('$msg');</script>";
-    }
-?>
